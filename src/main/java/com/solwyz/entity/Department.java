ff9eaf23f4729e551d5ba5024d9a6645300a516b
Solwyz <jismis@solwyz.com>
2025-05-30 17:38:50 +0530

    designation list

--- src/main/java/com/solwyz/entity/Department.java
+++ src/main/java/com/solwyz/entity/Department.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,7 +22,7 @@ public class Department {
 	private Long id;
 	private String name;
 
-	@OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Designation> designations;
 	
 	public Long getId() {
--- src/main/java/com/solwyz/repo/DesignationRepository.java
+++ src/main/java/com/solwyz/repo/DesignationRepository.java
@@ -1,5 +1,7 @@
 package com.solwyz.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.solwyz.entity.Designation;
 
 @Repository
 public interface DesignationRepository extends JpaRepository<Designation, Long>{
+
+	List<Designation> findByDepartmentId(Long departmentId);
 
 }
